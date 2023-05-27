2201c73ed96fad6753d17c0d002b43ade411b1ba
Joaquin Pelle <pelle.joaquin@gmail.com>
2023-05-26 21:57:24 -0300

    Serialize script

--- disk_bosonstar.jl
+++ disk_bosonstar.jl
@@ -1,43 +1,73 @@
+import Pkg
+Pkg.activate("BosonStars")
+# Pkg.resolve()
+
 using Skylight
 using CairoMakie
+using Printf
 
 include("bosonstar_parameters.jl")
 
-spacetime = BosonStarSpacetime(a=a_LBS1,b=b_LBS1)
+Nres = 1200
 
-image_plane = ImagePlane(distance = 500.0,
-                         observer_inclination_in_degrees = 85,
-                         horizontal_side_image_plane = 90.0,
-                         vertical_side_image_plane = 90.0,
-                         horizontal_number_of_nodes = 300,
-                         vertical_number_of_nodes = 300)
-
-model = BosonStarAccretionDisk(inner_radius=rin_LBS1, outer_radius=rout_LBS1, temperature_file="TempLBS1.dat")
+for modeltype in ["SBS"]
+    for modelid in [3]
         
-configurations = VacuumOTEConfigurations(spacetime=spacetime,
-                                         image_plane = image_plane,
-                                         observed_times = [0.0],
-                                         radiative_model = model,
-                                         unit_mass_in_solar_masses=1.0)
+        modelname = modeltype*string(modelid)
+        
+        a = eval(Symbol("a_$(modelname)"))
+        b = eval(Symbol("b_$(modelname)"))
+        rin = eval(Symbol("rin_$(modelname)"))
+        rout = eval(Symbol("rout_$(modelname)"))
+        tempfilename = "tables/Temp$(modelname).dat"
+        
+        spacetime = BosonStarSpacetime(a=a,b=b)
 
-initial_data = get_initial_data(configurations)
+        for ξ in [5, 45, 85]
 
-cb, cb_params = get_callback_and_params(configurations) #... or, define your own cb and cb_params
+            ξstr = string(@sprintf("%02d", ξ))
+            Nstr = string(@sprintf("%03d", Nres))
+            filename =  "$(modelname)_i$(ξstr)deg_N$(Nstr)"
+            
+            image_plane = ImagePlane(distance = 500.0,
+                                    observer_inclination_in_degrees = ξ,
+                                    horizontal_side_image_plane = rout*1.1,
+                                    vertical_side_image_plane = rout*1.1,
+                                    horizontal_number_of_nodes = Nres,
+                                    vertical_number_of_nodes = Nres)
 
-run = integrate(initial_data, configurations, cb, cb_params; method=VCABM(), reltol=1e-13, abstol=1e-21)
+            model = BosonStarAccretionDisk(inner_radius=rin, outer_radius=rout, temperature_file=tempfilename)
+                    
+            configurations = VacuumOTEConfigurations(spacetime=spacetime,
+                                                    image_plane = image_plane,
+                                                    observed_times = [0.0],
+                                                    radiative_model = model,
+                                                    unit_mass_in_solar_masses=1.0)
 
-output_data = output_data(run)
+            initial_data = get_initial_data(configurations)
 
-bolometric_intensities = get_observed_bolometric_intensities(initial_data, output_data, configurations)
+            cb, cb_params = get_callback_and_params(configurations) #... or, define your own cb and cb_params
 
-xs,ys = get_pixel_coordinates_vectors(configurations)
+            run = integrate(initial_data, configurations, cb, cb_params; method=VCABM(), reltol=1e-13, abstol=1e-21)
+            
+            output_data = run.output_data
 
-zs = view_intensities_grid(bolometric_intensities, configurations)
+            bolometric_intensities = get_observed_bolometric_intensities(initial_data, output_data, configurations)
 
-fig = Figure(font = "CMU Serif")
-ax = Axis(fig[1,1], xlabel=L"\alpha", ylabel=L"\beta", ylabelsize = 26, xlabelsize = 26) 
-hmap = heatmap!(xs, ys, zs; colormap=:gist_heat, interpolate=true)
-Colorbar(fig[:, end+1], hmap, label=L"I", labelsize=26, width = 15, ticksize = 18, tickalign = 1)
-colsize!(fig.layout, 1, Aspect(1, 1.0))
-colgap!(fig.layout, 7)
-CairoMakie.save("plot.png", fig)
+            xs,ys = get_pixel_coordinates_vectors(configurations)
+
+            zs = view_intensities_grid(bolometric_intensities, configurations)
+
+            fig = Figure(font = "CMU Serif")
+            ax = Axis(fig[1,1], xlabel=L"\alpha", ylabel=L"\beta", ylabelsize = 26, xlabelsize = 26) 
+            hmap = heatmap!(xs, ys, zs; colormap=:gist_heat, interpolate=true)
+            Colorbar(fig[:, end+1], hmap, label=L"I", labelsize=26, width = 15, ticksize = 18, tickalign = 1)
+            colsize!(fig.layout, 1, Aspect(1, 1.0))
+            colgap!(fig.layout, 7)
+
+            CairoMakie.save("plots/$(filename).png", fig)
+            save_to_hdf5("io/$(filename).h5", configurations, initial_data, [run])
+
+        end
+    end
+end
