92eb6427feb62b16e5f71d58cdb80df89ece47ae
joaquinpelle <pelle.joaquin@gmail.com>
2023-07-19 19:35:14 -0300

    up

--- disk_bosonstar.jl
+++ disk_bosonstar.jl
@@ -8,86 +8,87 @@ using Printf
 
 include("bosonstar_parameters.jl")
 
-# Nres = 1200
-Nres = 50
-num_bins = 40
+function main(obs_distance=1000.0, Nres=1200)
 
-for modeltype in ["LBS","SBS"]
-    for modelid in [1,2,3]
-        
-        modelname = modeltype*string(modelid)
-        
-        a = eval(Symbol("a_$(modelname)"))
-        b = eval(Symbol("b_$(modelname)"))
-        rin = eval(Symbol("rin_$(modelname)"))
-        rout = eval(Symbol("rout_$(modelname)"))
-        tempfilename = "tables/Temp$(modelname).dat"
-        
-        spacetime = BosonStarSpacetime(a=a,b=b)
-
-        for ξ in 5:5:85
-
-            ξstr = string(@sprintf("%02d", ξ))
-            Nstr = string(@sprintf("%03d", Nres))
-            filename =  "$(modelname)_i$(ξstr)deg_N$(Nstr)"
+    for modeltype in ["LBS","SBS"]
+        for modelid in [1,2,3]
             
-            camera = ImagePlane(distance = 500.0,
-                                    observer_inclination_in_degrees = ξ,
-                                    observation_times = [0.0],
-                                    horizontal_side_camera = rout*1.1,
-                                    vertical_side_camera = rout*1.1,
-                                    horizontal_number_of_nodes = Nres,
-                                    vertical_number_of_nodes = Nres)
-
-            model = AccretionDiskWithTabulatedTemperature(inner_radius=rin, outer_radius=rout, filename=tempfilename)
-                    
-            configurations = VacuumOTEConfigurations(spacetime=spacetime,
-                                                    camera = camera,
-                                                    radiative_model = model,
-                                                    unit_mass_in_solar_masses=1.0)
-
-            # initial_data = initialize(configurations)
-
-            # cb, cb_params = callback_setup(configurations) #... or, define your own cb and cb_params
-
-            # run = integrate(initial_data, configurations, cb, cb_params; method=VCABM(), reltol=1e-13, abstol=1e-21)
+            modelname = modeltype*string(modelid)
             
-            # output_data = run.output_data
+            a = eval(Symbol("a_$(modelname)"))
+            b = eval(Symbol("b_$(modelname)"))
+            rin = eval(Symbol("rin_$(modelname)"))
+            rout = eval(Symbol("rout_$(modelname)"))
+            tempfilename = "tables/Temp$(modelname).dat"
             
-            # save_to_hdf5("io/$(filename).h5", configurations, initial_data, [run])
-            initial_data = load_initial_data_from_hdf5("io/$(filename).h5")
-            # configurations = load_configurations_from_hdf5("io/$(filename).h5")
-            output_data = load_output_data_from_hdf5("io/$(filename).h5", 1)
-            
-            bolometric_intensities, q = observed_bolometric_intensities(initial_data, output_data, configurations)
-            xs,ys = axes_ranges(camera)
-            zs = grid_view(bolometric_intensities, configurations)
+            spacetime = BosonStarSpacetime(a=a,b=b)
 
-            joint_limits = (1e4,2e5)
-            fig = Figure()
-            ax = Axis(fig[1,1], xlabel=L"\alpha", ylabel=L"\beta", ylabelsize = 26, xlabelsize = 26) 
-            hmap = heatmap!(xs, ys, zs; colormap=:gist_heat, interpolate=true, colorrange=joint_limits)
-            Colorbar(fig[:, end+1], hmap, label=L"I", labelsize=26, width = 15, ticksize = 18, tickalign = 1)
-            colsize!(fig.layout, 1, Aspect(1, 1.0))
-            colgap!(fig.layout, 7)
-            CairoMakie.save("plots/image/$(filename).png", fig)
+            for ξ in [5,45,85]
 
-            # binned_fluxes, bins = line_emission_spectrum(initial_data, output_data, configurations; emission_profile = myprofile, num_bins = num_bins)
-            # max_flux = maximum(binned_fluxes)
-            # bins_midpoints = 0.5*(bins[1:end-1] + bins[2:end])
+                ξstr = string(@sprintf("%02d", ξ))
+                Nstr = string(@sprintf("%03d", Nres))
+                filename =  "$(modelname)_i$(ξstr)deg_N$(Nstr)"
+                
+                spher_pos = [0.0, obs_distance, deg2rad(ξ), 0.0]
 
-            # fig = Figure(resolution = (600, 400))
-            # ax = Axis(fig[1, 1], xlabel = L"E/E_0", ylabel = "Flux (arbitrary)", title = "Relativistic line broadening", titlefont=:regular)
-            # skl = lines!(ax, bins_midpoints, binned_fluxes/max_flux, linewidth = 3, color = :black)
+                if modeltype=="LBS"
+                    ap = 3.5
+                elseif modeltype=="SBS"
+                    ap = 5.5
+                end
 
-            # ax.titlesize = 22
-            # ax.xlabelsize = 22
-            # ax.ylabelsize = 22
-            # ax.xticklabelsize = 15
-            # ax.yticklabelsize = 15
+                camera = PinholeCamera(position = spher_pos,
+                                    horizontal_aperture_in_degrees = ap, #rad2deg(70/distance),
+                                    vertical_aperture_in_degrees = ap, #rad2deg(70/distance),
+                                    horizontal_number_of_pixels = Nres,
+                                    vertical_number_of_pixels = Nres) 
 
-            # # Save the figure
-            # CairoMakie.save("plots/line_broadening/$(filename)_bins$(num_bins).png", fig; dpi=300)
+                model = AccretionDiskWithTabulatedTemperature(inner_radius=rin, outer_radius=rout, filename=tempfilename)
+                        
+                configurations = VacuumOTEConfigurations(spacetime=spacetime,
+                                                        camera = camera,
+                                                        radiative_model = model,
+                                                        unit_mass_in_solar_masses=1.0)
+
+                initial_data = initialize(configurations)
+                cb, cb_params = callback_setup(configurations) #... or, define your own cb and cb_params
+                run = integrate(initial_data, configurations, cb, cb_params; method=VCABM(), reltol=1e-6, abstol=1e-6)
+                output_data = run.output_data
+                save_to_hdf5("io/$(filename).h5", configurations, initial_data, [run]; mode="w")
+
+                bolometric_intensities, q = observed_bolometric_intensities(initial_data, output_data, configurations)
+                xs,ys = axes_ranges(camera)
+                zs = grid_view(bolometric_intensities, configurations)
+
+                # joint_limits = (1e4,2e5)
+                fig = Figure()
+                ax = Axis(fig[1,1], xlabel=L"\alpha", ylabel=L"\beta", ylabelsize = 26, xlabelsize = 26) 
+                hmap = heatmap!(xs, ys, zs; colormap=:gist_heat, interpolate=true)#, colorrange=joint_limits)
+                Colorbar(fig[:, end+1], hmap, label=L"I", labelsize=26, width = 15, ticksize = 18, tickalign = 1)
+                colsize!(fig.layout, 1, Aspect(1, 1.0))
+                colgap!(fig.layout, 7)
+                CairoMakie.save("plots/image/$(filename).png", fig)
+
+                println("$(filename) done")
+                # binned_fluxes, bins = line_emission_spectrum(initial_data, output_data, configurations; emission_profile = myprofile, num_bins = num_bins)
+                # max_flux = maximum(binned_fluxes)
+                # bins_midpoints = 0.5*(bins[1:end-1] + bins[2:end])
+
+                # fig = Figure(resolution = (600, 400))
+                # ax = Axis(fig[1, 1], xlabel = L"E/E_0", ylabel = "Flux (arbitrary)", title = "Relativistic line broadening", titlefont=:regular)
+                # skl = lines!(ax, bins_midpoints, binned_fluxes/max_flux, linewidth = 3, color = :black)
+
+                # ax.titlesize = 22
+                # ax.xlabelsize = 22
+                # ax.ylabelsize = 22
+                # ax.xticklabelsize = 15
+                # ax.yticklabelsize = 15
+
+                # # Save the figure
+                # CairoMakie.save("plots/line_broadening/$(filename)_bins$(num_bins).png", fig; dpi=300)
+            end
         end
     end
 end
+
+main()
---
+++ submit_skylight.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+
+### Standard output and error:
+#SBATCH -o std/out.%j
+#SBATCH -e std/err.%j
+
+### Initial working directory:
+#SBATCH -D ./
+
+### Job Name:
+#SBATCH -J bosonstar
+
+#SBATCH --partition=multi
+
+### Number of nodes and MPI tasks per node:
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#
+###SBATCH --mail-type=ALL
+###SBATCH --mail-user=jpelle@mi.unc.edu.ar
+#
+# Wall clock Limit:
+#SBATCH --time=1:00:00
+
+##################################
+
+export JULIA_NUM_THREADS=64
+export SLURM_HINT=multithread 
+
+julia disk_bosonstar.jl
