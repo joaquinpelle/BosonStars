a1e894ca415490857552938dda4a529664177830
joaquinpelle <pelle.joaquin@gmail.com>
2024-02-16 16:05:42 -0300

    Remove line profiles from range functions

--- ranges.jl
+++ ranges.jl
@@ -56,31 +56,22 @@ function load_everything(filename)
     output_data = load_output_data_from_hdf5("io/$(filename).h5", 1)
     return initial_data, output_data, configurations
 end
-function ranges(filenames, num_bins)
-    qmin = Inf
-    qmax = -Inf
+function ranges(filenames)
     zmin = Inf
     zmax = -Inf
-    Fmax = -Inf
     for filename in filenames
         @assert isa(filename, AbstractString) "filenames must be an array of strings" 
-        qminv, qmaxv, zminv, zmaxv, Fmaxv = ranges(filename, num_bins)
-        qmin = min(qmin, qminv)
-        qmax = max(qmax, qmaxv)
+        zminv, zmaxv = ranges(filename)
         zmin = min(zmin, zminv)
         zmax = max(zmax, zmaxv)
-        Fmax = max(Fmax, Fmaxv)
     end
-    return qmin, qmax, zmin, zmax, Fmax
+    return zmin, zmax
 end
 
-function ranges(filename::AbstractString, num_bins)
+function ranges(filename::AbstractString)
 
     initial_data, output_data, configurations = load_everything(filename)
-    Iobs, q = observed_bolometric_intensities(initial_data, output_data, configurations)
-    qmin, qmax = extrema(q[q.!=0.0])
+    Iobs = observed_bolometric_intensities(initial_data, output_data, configurations)
     zmin, zmax = extrema(Iobs)
-
-    binned_fluxes, bins = line_emission_spectrum(initial_data, output_data, configurations; num_bins = num_bins)
-    return qmin, qmax, zmin, zmax, maximum(binned_fluxes)
+    return zmin, zmax 
 end
