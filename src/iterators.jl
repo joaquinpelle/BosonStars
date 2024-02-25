4c947370b68dcc158a92f05e379653aff7ff8858
joaquinpelle <pelle.joaquin@gmail.com>
2024-02-25 20:11:27 -0300

    Add getindex forwarding for models

--- src/iterators.jl
+++ src/iterators.jl
@@ -30,3 +30,6 @@ Base.IteratorSize(m::AbstractBosonStar) = Base.IteratorSize(m.id)
 Base.length(m::AbstractBosonStar) = Base.length(m.id)
 Base.isdone(m::AbstractBosonStar) = Base.isdone(m.id)
 Base.isdone(m::AbstractBosonStar, state) = Base.isdone(m.id, state)	
+
+Base.getindex(m::SBS{T}, i::Int) where {T<:CollectiveId} = SBS(m.id[i])
+Base.getindex(m::LBS{T}, i::Int) where {T<:CollectiveId} = LBS(m.id[i])
