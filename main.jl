712e824d4057938000def9eb90581931d11bb703
joaquinpelle <pelle.joaquin@gmail.com>
2024-02-25 23:04:13 -0300

    Update main file

--- main.jl
+++ main.jl
@@ -1,5 +1,6 @@
 using Pkg 
 Pkg.activate("BosonStars")
+using Parameters
 using CairoMakie
 using Printf
 using Colors
@@ -7,8 +8,57 @@ using DelimitedFiles
 using Skylight
 include("src/BosonStars.jl")
 
-models = create_model_set(LBS_ids=1:3, SBS_ids = 1:3, BH = true)
-params = CameraRunSet(models = models, inclinations = [5, 45, 85], number_of_pixels_per_side = 1200, observation_radius = 1000.0)
-make_runs(params; reltol=1e-6, abstol=1e-6)
-corona_params = CoronaRunSet(models = models, heights = [2.5, 5.0, 10.0], spectral_index = 2.0, number_of_packets = 5000000, number_of__radial_bins = 50)
-make_runs(corona_params; reltol=1e-5, abstol=1e-5)
+LBSmodels = LBS(1:3)
+SBSmodels = SBS(1:3)
+BHmodel = BH()
+
+LBSrunset = CameraRunSet(models = LBSmodels,
+                    inclinations = [5, 45, 85],
+                    number_of_pixels_per_side = 1200,
+                    observation_radius = 1000.0)
+
+SBSrunset = CameraRunSet(models = SBSmodels,
+                    inclinations = [5, 45, 85],
+                    number_of_pixels_per_side = 1200,
+                    observation_radius = 1000.0)
+
+BHrunset = CameraRunSet(models = BHmodel,
+                    inclinations = [5, 45, 85],
+                    number_of_pixels_per_side = 1200,
+                    observation_radius = 1000.0)
+
+make_runs([LBSrunset, SBSrunset, BHrunset]; reltol=1e-6, abstol=1e-6)
+
+LBScorona_runset = CoronaRunSet(models = LBSmodels,
+                    heights = [2.5, 5.0, 10.0],
+                    spectral_index = 2.0,
+                    number_of_packets = 5000000,
+                    number_of_radial_bins = 50)
+
+SBScorona_runset = CoronaRunSet(models = SBSmodels,
+                    heights = [2.5, 5.0, 10.0],
+                    spectral_index = 2.0,
+                    number_of_packets = 5000000,
+                    number_of_radial_bins = 50)
+
+BHcorona_runset = CoronaRunSet(models = BHmodel,
+                    heights = [2.5, 5.0, 10.0],
+                    spectral_index = 2.0,
+                    number_of_packets = 5000000,
+                    number_of_radial_bins = 50)
+
+make_runs([LBScorona_runset, SBScorona_runset, BHcorona_runset]; reltol=1e-5, abstol=1e-5)
+
+_, zmax = zextrema([LBSrunset, SBSrunset, BHrunset])
+bolometric_intensity_mosaic(LBSrunset; zmax = zmax, figname = "plots/LBS_mosaic.pdf")
+bolometric_intensity_mosaic(SBSrunset; zmax = zmax, figname = "plots/SBS_mosaic.pdf")
+bolometric_intensity_mosaic(BHrunset; zmax = zmax, figname = "plots/BH_mosaic.pdf")
+
+ε = eV_to_erg(1.0)
+observation_energies = ε*exp10.(range(0.0, stop=4.0, length=20))
+spectrum_mosaic(LBSrunset, SBSrunset, BHrunset; observation_energies = observation_energies, figname = "plots/spectrum_mosaic.pdf")
+
+emissiviy_profile_mosaic(LBScorona_runset, SBScorona_runset, BHcorona_runset; figname = "plots/emissivity_mosaic.pdf")
+emissiviy_profile_mosaic_focused(SBScorona_runset, BHcorona_runset; figname = "plots/emissivity_mosaic_focused.pdf")
+
+line_emission_mosaic(SBSrunset, BHrunset, SBScorona_runset, BHcorona_runset; number_of_energy_bins = 40, figname = "plots/line_emission_mosaic.pdf")
