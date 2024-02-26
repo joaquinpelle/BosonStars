c6387734f3a5f856f30d0b97a880a5157eb5d20b
joaquinpelle <pelle.joaquin@gmail.com>
2024-02-25 23:15:42 -0300

    Refactor filename functions

--- src/filenames.jl
+++ src/filenames.jl
@@ -1,23 +1,16 @@
-to_string(model::SBS{Int}) = "SBS$(model.id)"
-to_string(model::LBS{Int}) = "LBS$(model.id)"
-to_symbol(model::AbstractBosonStar) = (Symbol ∘ to_string)(model)
-to_symbol(::BH) = :SCHW
+datafile(params::CameraRunParams) = camera_file(basename(params))
+datafile(params::CoronaRunParams) = corona_file(basename(params))
 
-modeldir(::AbstractBosonStar) = "bosonstar"
-modeldir(::BH) = "schwarzschild"
+camera_file(basename::AbstractString) = "io/camera/$(basename).h5"
+corona_file(basename::AbstractString) = "io/corona/$(basename).txt"
+temperature_file(model::AbstractModel) = "io/temperature/Temp$(to_string(model)).dat"
 
 function basename(params::CameraRunParams)
+    dir = modeldir(params.model)
     modelsymbol = to_symbol(params.model)
     ξstr = string(@sprintf("%02d", params.inclination)) 
     Nstr = string(@sprintf("%04d", params.number_of_pixels_per_side))
-    return "$(modelsymbol)_i$(ξstr)deg_N$(Nstr)"
-end
-
-function basename(params::CoronaRunParams)
-    modelsymbol = to_symbol(params.model)
-    ξstr = string(@sprintf("%02d", params.inclination)) 
-    Nstr = string(@sprintf("%04d", params.number_of_pixels_per_side))
-    return "$(modelsymbol)_i$(ξstr)deg_N$(Nstr)"
+    return "$(dir)/$(modelsymbol)_i$(ξstr)deg_N$(Nstr)"
 end
 
 function basename(params::CoronaRunParams)
@@ -28,8 +21,10 @@ function basename(params::CoronaRunParams)
     return "$(dir)/$(modelsymbol)_h$(hstr)_idx$(istr)"
 end
 
-datafile(params::CameraRunParams) = datafile(basename(params))
-datafile(params::CoronaRunParams) = corona_file(basename(params))
-datafile(basename::AbstractString) = "io/$(basename).h5"
-corona_file(basename::AbstractString) = "io/corona/$(basename).txt"
-temperature_file(model::AbstractModel) = "io/temperature/Temp$(to_string(model)).dat"
+modeldir(::AbstractBosonStar) = "bosonstar"
+modeldir(::BH) = "schwarzschild"
+
+to_string(model::SBS{Int}) = "SBS$(model.id)"
+to_string(model::LBS{Int}) = "LBS$(model.id)"
+to_symbol(model::AbstractBosonStar) = (Symbol ∘ to_string)(model)
+to_symbol(::BH) = :SCHW
