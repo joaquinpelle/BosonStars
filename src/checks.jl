ecf640ee6ee8c9072c5a2e6061faa89f0ebd0e93
joaquinpelle <pelle.joaquin@gmail.com>
2024-02-26 00:25:47 -0300

    Remove blank line

--- src/checks.jl
+++ src/checks.jl
@@ -12,6 +12,5 @@ have_three_heights(runsets::Vector{T}) where {T<:CoronaRunSet} = all(has_three_h
 have_same_models(runsets::Vector{T}) where {T<:AbstractRunSet} = all(r -> r.models == runsets[1].models, runsets)
 have_same_inclinations(runsets::Vector{T}) where {T<:CameraRunSet} = all(r -> r.inclinations == runsets[1].inclinations, runsets)
 have_same_heights(runsets::Vector{T}) where {T<:CoronaRunSet} = all(r -> r.heights == runsets[1].heights, runsets)
-
 have_three_primary_parameters(runsets::Vector{T}) where {T<:AbstractRunSet} = all(r -> length(primary_parameter(r)) == 3, runsets)
 have_same_primary_parameters(runsets::Vector{T}) where {T<:AbstractRunSet} = all(r -> primary_parameter(r) == primary_parameter(runsets[1]), runsets)
